17a8f08084fbf3b39131b133bf8ca94854a99668
John T. Foster <johntfosterjr@gmail.com>
2018-09-26 23:01:56 -0500

    Update for 2018

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
-from assignment8 import Toughness
 
 import numpy as np
+import unittest
 
-def test_toughness_1():
-    
-    ss = Toughness('data_1.dat')
-    
-    np.testing.assert_allclose(ss.compute_toughness_simps(), 70836.23, atol=0.01)
-    np.testing.assert_allclose(ss.compute_toughness_trapz(), 70836.14, atol=0.01)
-    
-def test_toughness_2():
-    
-    ss = Toughness('data_2.dat')
-    
-    np.testing.assert_allclose(ss.compute_toughness_simps(), 42744.49, atol=0.01)
-    np.testing.assert_allclose(ss.compute_toughness_trapz(), 42744.44, atol=0.01)
+
+from assignment8 import Toughness
+
+class TestSolution(unittest.TestCase):
+
+    def test_toughness(self):
+
+        ss = Toughness('data.dat')
+
+        np.testing.assert_allclose(ss.compute_toughness_simps(), 70836.23, atol=0.01)
+        np.testing.assert_allclose(ss.compute_toughness_trapz(), 70836.14, atol=0.01)
+
+    def test_toughness_private(self):
+
+        ss = Toughness('data_private.dat')
+
+        np.testing.assert_allclose(ss.compute_toughness_simps(), 42744.49, atol=0.01)
+        np.testing.assert_allclose(ss.compute_toughness_trapz(), 42744.44, atol=0.01)
+        
+if __name__ == '__main__':
+    unittest.main()
 
